7105d599319934f9c63e28833a3469fb2537e917
Milyn <milyng.dsilva@gmail.com>
2021-10-19 09:52:16 +0530

    Added sample output

--- Hash.js
+++ Hash.js
@@ -2,10 +2,10 @@ function hash(string) {
     let hashing ="";
     for(let i=0;i<string.length;i++) {
     hashing +=string.charCodeAt(i)
-
 }
 return hashing;
 }
 console.log(hash("Name"))
 
 //The charCodeAt() method returns an integer between 0 and 65535 representing the UTF-16 code unit at the given index.
+//Sample Output :7897109101
