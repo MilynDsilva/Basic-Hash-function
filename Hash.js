5d2f32ab89acd2741f7ac8012643954a99ad1a94
Milyn <milyng.dsilva@gmail.com>
2021-10-19 09:48:36 +0530

    added a hash function

---
+++ Hash.js
@@ -0,0 +1,11 @@
+function hash(string) {
+    let hashing ="";
+    for(let i=0;i<string.length;i++) {
+    hashing +=string.charCodeAt(i)
+
+}
+return hashing;
+}
+console.log(hash("Name"))
+
+//The charCodeAt() method returns an integer between 0 and 65535 representing the UTF-16 code unit at the given index.
